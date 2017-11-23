5b8310c902172840b504f2651994f495994b96e4
Bob Toney <bobctoney@gmail.com>
2017-11-22 23:40:36 -0800

    Add example tests for modal

--- setup_tests.js
+++ setup_tests.js
@@ -1,14 +1,18 @@
+const { configure } = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
 const jsdom = require('jsdom');
+
+// JSDOM
 const { window } = new jsdom.JSDOM('');
-
 global.window = window;
-
 Object.keys(window).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     global[property] = window[property];
   }
 });
-
 global.navigator = {
   userAgent: 'node.js'
 };
+
+// ENZYME
+configure({ adapter: new Adapter() });
--- src/components/modal/modal.test.js
+++ src/components/modal/modal.test.js
@@ -1,8 +1,53 @@
+import React from 'react';
 import { expect } from'chai'
+import { mount } from 'enzyme';
+import sinon from 'sinon';
 import Modal from './index';
 
-describe('Modal test', function () {
-  it('example unit test', function () {
-    expect(true).to.equal(true);
+describe('Modal test', () => {
+  let onClose, wrapper, clock;
+
+  beforeEach(() => {
+    onClose = sinon.spy();
+    wrapper = mount(
+      <Modal onClose={onClose}>
+        <div id='test-modal'>Modal</div>
+      </Modal>
+    );
+    clock = sinon.useFakeTimers();
   });
+
+  afterEach(() => {
+    clock.restore()
+  });
+
+  it('Renders children and calls onClose on outside click', () => {
+    expect(wrapper.find('#test-modal')).to.have.length(1);
+
+    const modalWrapper = wrapper.find('.modal-wrapper');
+    expect(modalWrapper).to.have.length(1);
+    modalWrapper.simulate('click');
+    clock.tick(500);
+    expect(onClose.callCount).to.equal(1);
+  });
+
+  it('Doesn\'t call onClose when clicking inside modal content', () => {
+    wrapper.find('#test-modal').simulate('click');
+    clock.tick(500);
+    expect(onClose.callCount).to.equal(0);
+  });
+
+  it('Renders children as function and calls onclose', () => {
+    wrapper = mount(
+      <Modal onClose={onClose}>
+        {({ close }) => <div id='test-close' onClick={close}>Close</div>}
+      </Modal>
+    );
+
+    const closeEl = wrapper.find('#test-close');
+    expect(closeEl).to.have.length(1);
+    closeEl.simulate('click');
+    clock.tick(500);
+    expect(onClose.callCount).to.equal(1);
+  })
 });
